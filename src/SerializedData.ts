e55ddc646ae57bf877edeeba13cac5f6d04c30ad
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2020-09-01 19:42:49 +0200

    Added basic functionality to serializeddata

--- src/SerializedData.ts
+++ src/SerializedData.ts
@@ -6,4 +6,16 @@ export class SerializedData {
         this.json = jsonElement || {}
     }
 
+    applyAs<T>(field?: string): T {
+        return field ? this.json[field] : this.json;
+    }
+
+    getAsObject(field?: string): object {
+        return field ? this.json[field] : this.json;
+    }
+
+    write(field: string, object: Object) {
+        this.json[field] = object;
+    }
+
 }
