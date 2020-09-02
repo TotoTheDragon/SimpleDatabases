a541691b93defa8ac15bae0ff4c393c8bbd7c230
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2020-09-02 11:33:58 +0200

    Added a GetByType method for the storageholder

--- src/StorageHolder.ts
+++ src/StorageHolder.ts
@@ -1,6 +1,8 @@
+import { SqlDataBody } from "./body/SqlDataBody";
 import { Loadable } from "./util/Loadable";
 import { Saveable } from "./util/Saveable";
 import { Storage } from "./storage/Storage";
+import { DataBody } from "./body/DataBody";
 
 export class StorageHolder implements Loadable, Saveable {
 
@@ -12,6 +14,10 @@ export class StorageHolder implements Loadable, Saveable {
 
     registerStorage(storage: Storage<any>) {
         this.storages.add(storage);
+    }
+
+    getByType<T extends DataBody>(dummy: T): Storage<T> {
+        return Array.from(this.storages).find(stora => typeof stora.getDummy() == typeof dummy);
     }
 
     save(callback?: () => any): Promise<void[]> {
