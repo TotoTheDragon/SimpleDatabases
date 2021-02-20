6d2fb6582e98a9321b9f7ade4456ed100bbadb0a
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-20 02:01:59 +0100

    Updated exports to allow for nice packaging

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 export * from "./objects/SerializableObject";
 export * from "./objects/SerializedData";
 
+export * from "./storage/AbstractStorage";
+export * from "./storage/sql/MySQLStorage";
+
+export * from "./databases/AbstractDatabase";
+export * from "./databases/sql/MySQLDatabase";
+
+export * from "./body/AbstractBody";
+export * from "./body/sql/MySQLBody";
+
 export * from "./util/Util";
+export * from "./util/Constants";
